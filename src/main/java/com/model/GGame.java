4a4744fbdf929ab9f2e13f2325614391732383d2
zephye030 <zephyr0_0@126.com>
2016-05-25 10:56:02 +0800

    微信页面

---
+++ src/main/java/com/model/GGame.java
@@ -0,0 +1,45 @@
+package com.model;
+
+import java.util.Date;
+
+public class GGame {
+    private Long id;
+
+    private Long userid;
+
+    private Long point;
+
+    private Date createTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserid() {
+        return userid;
+    }
+
+    public void setUserid(Long userid) {
+        this.userid = userid;
+    }
+
+    public Long getPoint() {
+        return point;
+    }
+
+    public void setPoint(Long point) {
+        this.point = point;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
